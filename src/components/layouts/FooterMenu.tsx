640793235f9fa0a351ec82aa04c2e8abbbb4a6fe
yamadahiroki <hirockysan1983@gmail.com>
2021-07-19 02:30:59 +0900

    ♻️ footer menuをmapで処理

--- src/components/layouts/FooterMenu.tsx
+++ src/components/layouts/FooterMenu.tsx
@@ -5,27 +5,41 @@ import { AiTwotoneHome, AiFillQuestionCircle } from "react-icons/ai";
 import { BsFillChatDotsFill } from "react-icons/bs";
 import { HiOutlineBookOpen } from "react-icons/hi";
 
+const FOOTERITEMS = [
+  {
+    element: <AiTwotoneHome className=" w-8 h-8 m-auto" />,
+    description: "ホーム",
+  },
+  {
+    element: <BsFillChatDotsFill className=" w-8 h-8 m-auto" />,
+    description: "つぶやく",
+  },
+  {
+    element: <AiFillQuestionCircle className=" w-8 h-8 m-auto" />,
+    description: "質問",
+  },
+  {
+    element: <HiOutlineBookOpen className=" w-8 h-8 m-auto" />,
+    description: "ブログ",
+  },
+];
+
 export const FooterMenu: FC = () => {
   return (
     <>
       <div className="border border-gray-300"></div>
       <div className="flex  justify-between mx-10 my-4">
-        <div className="cursor-pointer box-border m-auto text-blue-500 hover:text-blue-400">
-          <AiTwotoneHome className=" w-8 h-8" />
-          <p>ホーム</p>
-        </div>
-        <div className="cursor-pointer box-border m-auto text-blue-500 hover:text-blue-400">
-          <BsFillChatDotsFill className=" w-8 h-8" />
-          <p>つぶやく</p>
-        </div>
-        <div className="cursor-pointer box-border m-auto text-blue-500 hover:text-blue-400">
-          <AiFillQuestionCircle className=" w-8 h-8" />
-          <p>質問</p>
-        </div>
-        <div className="cursor-pointer box-border m-auto text-blue-500 hover:text-blue-400">
-          <HiOutlineBookOpen className=" w-8 h-8" />
-          <p>ブログ</p>
-        </div>
+        {FOOTERITEMS.map((item) => {
+          return (
+            <div
+              key={item.description}
+              className="cursor-pointer box-border m-auto text-blue-500 hover:text-blue-400"
+            >
+              {item.element}
+              <p>{item.description}</p>
+            </div>
+          );
+        })}
       </div>
     </>
   );
