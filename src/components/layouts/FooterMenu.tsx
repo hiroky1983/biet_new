c27149d783544f59bb098cd6a4c2e72478ce4c0e
yamadahiroki <hirockysan1983@gmail.com>
2021-07-19 14:18:54 +0900

    先程と同じ

--- src/components/layouts/FooterMenu.tsx
+++ src/components/layouts/FooterMenu.tsx
@@ -1,28 +1,38 @@
 import React, { FC } from "react";
 import "tailwindcss/tailwind.css";
+import Link from "next/link";
 
-import { AiTwotoneHome, AiFillQuestionCircle, AiOutlineSearch } from "react-icons/ai";
+import {
+  AiTwotoneHome,
+  AiFillQuestionCircle,
+  AiOutlineSearch,
+} from "react-icons/ai";
 import { BsFillChatDotsFill } from "react-icons/bs";
 import { HiOutlineBookOpen } from "react-icons/hi";
 
 const FOOTERITEMS = [
   {
+    href: "/",
     element: <AiTwotoneHome className=" w-8 h-8 m-auto" />,
     description: "ホーム",
   },
   {
+    href: "/",
     element: <BsFillChatDotsFill className=" w-8 h-8 m-auto" />,
     description: "ひとこと",
   },
   {
+    href: "/",
     element: <AiFillQuestionCircle className=" w-8 h-8 m-auto" />,
     description: "質問",
   },
   {
+    href: "/",
     element: <HiOutlineBookOpen className=" w-8 h-8 m-auto" />,
     description: "ブログ",
   },
   {
+    href: "/",
     element: <AiOutlineSearch className=" w-8 h-8 m-auto" />,
     description: "探す",
   },
@@ -35,13 +45,16 @@ export const FooterMenu: FC = () => {
       <div className="flex  justify-between mx-10 my-4">
         {FOOTERITEMS.map((item) => {
           return (
-            <div
-              key={item.description}
-              className="cursor-pointer box-border m-auto text-blue-500 hover:text-blue-400"
-            >
-              {item.element}
-              <p>{item.description}</p>
-            </div>
+            <Link href={item.href} key={item.href}>
+              <a>
+                <div
+                  className="cursor-pointer box-border m-auto text-blue-500 hover:text-blue-400"
+                >
+                  {item.element}
+                  <p>{item.description}</p>
+                </div>
+              </a>
+            </Link>
           );
         })}
       </div>
