0e46cc8129860de232a0781b0d54b541c8b37bf0
yamadahiroki <hirockysan1983@gmail.com>
2021-07-20 21:05:43 +0900

    コンテンツ追加

--- src/components/layouts/contents/ContentsLayout.tsx
+++ src/components/layouts/contents/ContentsLayout.tsx
@@ -6,20 +6,32 @@ import { ContentsCard } from "./ContentsCard";
 export const ContentsLayout: FC = () => {
   return (
     <div>
-      <h2 className="px-14 py-4">最新のつぶやき</h2>
+      <h1 className="px-14 py-4 font">#最新のつぶやき</h1>
       <div className="flex justify-between mx-6">
         <ContentsCard />
         <ContentsCard />
         <ContentsCard />
+        <ContentsCard />
       </div>
-      <div className="my-12 justify-center items-center">
+      <div className="my-10 justify-center items-center">
         <PrimaryButton>もっと見る</PrimaryButton>
       </div>
-      <h2 className="px-14 py-4">最新のつぶやき</h2>
+      <h1 className="px-14 py-4 font">#質問を見る</h1>
       <div className="flex justify-between mx-6">
         <ContentsCard />
         <ContentsCard />
         <ContentsCard />
+      </div>
+      <div className="my-10 justify-center items-center">
+        <PrimaryButton>もっと見る</PrimaryButton>
+      </div>
+      <h1 className="px-14 py-4 font">#最新のブログ</h1>
+      <div className="flex justify-between mx-6">
+
+        <ContentsCard />
+      </div>
+      <div className="my-10 justify-center items-center">
+        <PrimaryButton>もっと見る</PrimaryButton>
       </div>
     </div>
   );
