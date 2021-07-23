6246cac992842efc03441b1b7d4f58c305efed3a
yamadahiroki <hirockysan1983@gmail.com>
2021-07-24 00:58:48 +0900

    🚚 pathの変更とFC　→　VFCに変更

--- src/components/layouts/contents/ContentsLayout.tsx
+++ src/components/layouts/contents/ContentsLayout.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
-import React, { FC } from "react";
+import React, { VFC } from "react";
 import "tailwindcss/tailwind.css";
 import { PrimaryButton } from "../../button/PrimaryButton";
 import { ContentsCard } from "./ContentsCard";
 
-export const ContentsLayout: FC = () => {
+export const ContentsLayout: VFC = () => {
   return (
     <div>
       <h1 className="px-14 py-4 font-bold text-xl">#最新のつぶやき</h1>
-      <div className="flex  flex-wrap mx-6 gap-x-3">
+      <div className="justify-center sm:flex mx-6 gap-x-3">
         <ContentsCard />
         <div className="hidden lg:flex">
           <ContentsCard />
@@ -25,7 +25,7 @@ export const ContentsLayout: FC = () => {
         </Link>
       </div>
       <h1 className="px-14 py-4 font-bold text-xl">#質問を見る</h1>
-      <div className="flex  flex-wrap mx-6 gap-x-3">
+      <div className="justify-center sm:flex mx-6 gap-x-3">
         <ContentsCard />
         <div className="hidden lg:flex">
           <ContentsCard />
@@ -42,7 +42,7 @@ export const ContentsLayout: FC = () => {
         </Link>
       </div>
       <h1 className="px-14 py-4 font-bold text-xl">#最新のブログ</h1>
-      <div className="flex  flex-wrap mx-6 gap-x-3">
+      <div className="justify-center sm:flex mx-6 gap-x-3">
         <ContentsCard />
         <div className="hidden lg:flex">
           <ContentsCard />
