026eeb45fdd731409175f36dadb45f5019151437
yamadahiroki <hirockysan1983@gmail.com>
2021-07-31 23:25:18 +0900

    要素の削減

--- src/components/layouts/contents/ContentsLayout.tsx
+++ src/components/layouts/contents/ContentsLayout.tsx
@@ -7,23 +7,6 @@ import { ContentsCard } from "./ContentsCard";
 export const ContentsLayout: VFC = () => {
   return (
     <div>
-      <h1 className="px-14 py-4 font-bold text-xl">#最新のつぶやき</h1>
-      <div className="justify-center sm:flex mx-6 gap-x-3">
-        <ContentsCard />
-        <div className="hidden lg:flex">
-          <ContentsCard />
-        </div>
-        <div className="hidden sm:flex">
-          <ContentsCard />
-        </div>
-      </div>
-      <div className="my-10 justify-center items-center">
-        <Link href="/word">
-          <a>
-            <PrimaryButton>もっと見る</PrimaryButton>
-          </a>
-        </Link>
-      </div>
       <h1 className="px-14 py-4 font-bold text-xl">#質問を見る</h1>
       <div className="justify-center sm:flex mx-6 gap-x-3">
         <ContentsCard />
--- src/pages/question.tsx
+++ src/pages/question.tsx
@@ -5,9 +5,28 @@ import { BrowserFooterMenu } from "../components/layouts/footer/BrowserFooterMen
 import { MobileFooterMenu } from "../components/layouts/footer/MobileFooterMenu";
 import { SubHeader } from "../components/layouts/header/SubHeader";
 
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { PostCard } from "../components/layouts/contents/PostCard";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
+
 const Question: VFC = () => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen w-full">
       <Head>
         <title>biet-new</title>
         <meta
@@ -20,7 +39,21 @@ const Question: VFC = () => {
       <div className="flex">
         <div className="hidden lg:flex">
           <BrowserFooterMenu />
-          
+        </div>
+        <div className=" w-full h-screen">
+          <Paper className={classes.root}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              centered
+            >
+              <Tab label="質問する"></Tab>
+              <Tab label="質問を見る"></Tab>
+            </Tabs>
+            <PostCard />
+          </Paper>
         </div>
       </div>
       <div className="block lg:hidden">
