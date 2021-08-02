a05731728051162d934fdc623916f2f25201279f
yamadahiroki <hirockysan1983@gmail.com>
2021-08-02 15:24:05 +0900

    🌱 プロフィール変更用のbuttonを作成

---
+++ src/components/button/SecondaryButton.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode, VFC } from "react";
+
+type Props = {
+  children?: ReactNode;
+  // onClick?: () => void;
+};
+
+export const SecondaryButton: VFC<Props> = (props) => {
+  const { children } = props;
+  return (
+    <div className="text-center ">
+      <button className="bg-blue-500 text-white hover:bg-blue-400 focus:outline-none w-20 h-10 rounded-full">
+        {children}
+      </button>
+    </div>
+  );
+};
