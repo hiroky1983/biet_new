861b06c4b61bd1efe6af99d01ffd3c3e38b142a6
yamadahiroki <hirockysan1983@gmail.com>
2021-08-05 07:39:14 +0900

    ♻️ 不要なコードの削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 import Head from "next/head";
 import "tailwindcss/tailwind.css";
 import { ContentsLayout } from "../components/layouts/contents/ContentsLayout";
-import { MobileFooterMenu } from "../components/layouts/footer/MobileFooterMenu";
 import { Header } from "../components/layouts/header/Header";
 import { Profile } from "../components/layouts/Profile";
-import { BrowserFooterMenu } from "../components/layouts/footer/BrowserFooterMenu";
 import { Layout } from "../components/layouts/Layout";
-import Link from "next/link";
 
 export default function Home(): JSX.Element {
   return (
