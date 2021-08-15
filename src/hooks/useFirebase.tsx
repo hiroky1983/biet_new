b24b0e42ae06ab810b6886e3c1c6b94720c80e90
yamadahiroki <hirockysan1983@gmail.com>
2021-08-15 23:09:40 +0900

    🌱 hookファイルの追加

---
+++ src/hooks/useFirebase.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { db } from '../../firebase';
+import { selectUser } from '../lib/auth';
+
+
+export const useFirebase = () => {
+  const user = useSelector(selectUser)
+  
+  const docId = user.uid;
+  const photoUrl = user.photoUrl;
+  const userName = user.displayName;
+  // const checkValue = db.collection('users').doc(docId).get();
+  // const lang = user.language;
+
+  //  const userData = db.collection("users").doc(docId).set({
+  //   username: userName,
+  //   checkValue: checkValue,
+  //   lang: lang,
+  //   userstatus: userStatus,
+  // });
+
+  return {
+    docId,
+    photoUrl,
+    userName,
+
+  }
+}
