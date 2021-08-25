d45f7df00c5cec18f061d636f6a9ef09f3638137
yamadahiroki <hirockysan1983@gmail.com>
2021-08-25 10:20:30 +0900

    ➕ chakura UIの導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,16 @@ import React, { useEffect } from "react";
 import { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../lib/store";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
+      <ChakraProvider>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </ChakraProvider>
     </>
   );
 }
