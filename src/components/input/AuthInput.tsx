d9d8a89ea8636b394250d282e6927503e39b3ab9
yamadahiroki <hirockysan1983@gmail.com>
2021-09-21 08:26:34 +0900

    ✨ chakura導入でprops変更

--- src/components/input/AuthInput.tsx
+++ src/components/input/AuthInput.tsx
@@ -3,36 +3,48 @@ import { Input } from "@chakra-ui/react";
 
 type PROPS = {
   placeholder: string;
-  value: string;
-  onChange: InputHTMLAttributes<HTMLInputElement>["onChange"];
-  inputName?: string;
+  value?: string;
+  onChange?: InputHTMLAttributes<HTMLInputElement>["onChange"];
+  name?: string;
   type: string;
   autoComplete?: string;
   defaultValue?: string;
+  register?: any;
+  isReadOnly?: boolean;
+  isRequired?: boolean;
+  isInvalid?: boolean;
 };
 export const AuthInput: VFC<PROPS> = (props) => {
   const {
     placeholder,
     value,
     onChange,
-    inputName,
+    name,
     type,
     autoComplete,
     defaultValue,
+    register,
+    isReadOnly,
+    isRequired, 
+    isInvalid,
   } = props;
   return (
     <div className="mt-4">
       <Input
+        {...register}
         padding="2"
         type={type}
         autoComplete={autoComplete}
-        name={inputName}
+        name={name}
         variant="flushed"
         placeholder={placeholder}
         required
         value={value}
         onChange={onChange}
         defaultValue={defaultValue}
+        isReadOnly={isReadOnly}
+        isRequired={isRequired}
+        isInvalid={isInvalid}
       />
     </div>
   );
