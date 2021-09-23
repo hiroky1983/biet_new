2246d787738ba59d03409f84f605cebd1fcec31b
yamadahiroki <hirockysan1983@gmail.com>
2021-09-24 07:41:21 +0900

    commit

--- src/hooks/useFirebase.tsx
+++ src/hooks/useFirebase.tsx
@@ -3,16 +3,27 @@ import { useSelector } from 'react-redux';
 import { db } from '../../firebase';
 import { selectUser } from '../lib/auth';
 
-export const useFirebase = () => {
+export const useFirebase = async () => {
   const user = useSelector(selectUser)
-  
   const docId = user.uid;
   const photoUrl = user.photoUrl;
   const userName = user.displayName;
+  const docRef =  await db.collection("users").doc(docId).get();
+  const docData = async() =>  docRef.data();
+  const userData = {
+    userName: docData().userName,
+
+  }
 
   return {
     docId,
     photoUrl,
     userName,
+    docData,
+    // lang,
+    // userStatus,
+    // gender,
+    // avatarImage,
+    
   }
 }
