fc147fa370bc422e62018f1ff19879b3608360b9
yamadahiroki <hirockysan1983@gmail.com>
2021-10-07 21:58:06 +0900

    不要なファイルの削除

--- src/utils/buttonStyles.ts
+++
@@ -1,7 +0,0 @@
-export const buttonStyles = {
-  className:{
-    primary: "bg-blue-700 text-white hover:bg-blue-400 focus:outline-none w-20 h-10 rounded-full",
-    secondary: "bg-gray-700 text-white rounded-lg  hover:bg-gray-300 focus:outline-none w-32 h-16",
-    auth: "group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-400 focus:outline-none",
-  }
-}
