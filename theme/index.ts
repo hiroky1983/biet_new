5ebfe5623ff13979aff6034ff15f092bdf61186b
yamadahiroki <hirockysan1983@gmail.com>
2021-10-12 21:15:08 +0900

    🎨 グローバルテーマを定義

--- theme/index.ts
+++ theme/index.ts
@@ -3,23 +3,10 @@ export const theme = extendTheme({
   styles: {
     global: {
       body: {
-        bg: "#F2F2F2",
+        bg: "#F5F5F5",
         color: "#2E2E2E",
       },
     },
-  },
-  colors: {
-    green: "#639A67",
-    "green.500": "#639A67", // alertの色用(名前を変えないでください)
-    hoverPrimary: "#538A57", // ボタンホバー時用
-    hoverSecondary: "#f7fcf7",
-    hoverSidebar: "#89b38c",
-    yellow: "#ECC94B",
-  },
-  red: "#FB3F03",
-  blue: {
-    main: "#1D4ED8",
-    light: "#0085FF",
   },
   components: {
     Input: {
@@ -29,9 +16,7 @@ export const theme = extendTheme({
     },
     Button: {
       baseStyle: {
-        _focus: {
-          boxShadow: "0px 0px 0px 2px #89B38C",
-        },
+        bg: "#2E2E2E",
       },
     },
   },
