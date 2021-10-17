a1269dca18cca2a8ceef36005c4e1e7c3f69bee2
yamadahiroki <hirockysan1983@gmail.com>
2021-10-17 09:09:41 +0900

    ✨ タブメニューの実装変更

--- src/layouts/contents/TabMenu.tsx
+++ src/layouts/contents/TabMenu.tsx
@@ -1,105 +1,30 @@
-import React, { JSXElementConstructor, VFC } from "react";
-import { makeStyles, Theme } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
+import { useState, VFC } from "react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Box } from "@chakra-ui/react";
 import { PostCard } from "./PostCard";
-import { useState } from "react";
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: any;
-  value: any;
-}
-
-export const TabPanel: VFC<TabPanelProps> = (porps) => {
-  const { children, index, value, ...other } = porps;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`nav-tabpanel-${index}`}
-      aria-labelledby={`nav-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-};
-
-function a11yProps(index: any) {
-  return {
-    id: `nav-tab-${index}`,
-    "aria-controls": `nav-tabpanel-${index}`,
-  };
-}
-
-interface LinkTabProps {
-  label?: string;
-  href?: string;
-}
-
-function LinkTab(props: LinkTabProps) {
-  return (
-    <Tab
-      component="a"
-      onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-        event.preventDefault();
-      }}
-      {...props}
-    />
-  );
-}
-
-const useStyles = makeStyles(() => ({
-  root: {
-    flexGrow: 1,
-  },
-  line: {
-    color: "#374251",
-    boxShadow: "0 0px 0px 0px rgba(0, 0, 0, 0)",
-  },
-}));
 
 export const TabMenu: VFC = () => {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
   const [posts, setPosts] = useState<Array<any>>([]);
-
-  const handleChange = (e: React.ChangeEvent<{}>, newValue: number) => {
-    setValue(newValue);
-  };
-
   return (
-    <div className="h-screen">
-      <div className={classes.root}>
-        <AppBar position="static" color="inherit" className={classes.line}>
-          <Tabs
-            variant="fullWidth"
-            value={value}
-            onChange={handleChange}
-            aria-label="nav tabs"
-            indicatorColor="primary"
-          >
-            <LinkTab label="質問をする" href="/drafts" {...a11yProps(0)} />
-            <LinkTab label="質問を見る" href="/trash" {...a11yProps(1)} />
-          </Tabs>
-        </AppBar>
-        <TabPanel value={value} index={0}>
-          <div className="h-auto bottom-4 fixed  -translate-x-1/2 text-right float-right mr-6">
-          </div>
+    <Tabs isFitted variant="enclosed">
+      <TabList mb="1em">
+        <Tab>質問を投稿する</Tab>
+        <Tab>質問に回答する</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <Box>
+            <PostCard />
+            <PostCard />
+          </Box>
         </TabPanel>
-        <TabPanel value={value} index={1}>
-          {posts.length === 0 ? <p className="text-gray-700">質問はまだありません</p> : <PostCard />}
+        <TabPanel>
+          {posts.length === 0 ? (
+            <p className="text-gray-700">質問はまだありません</p>
+          ) : (
+            <PostCard />
+          )}
         </TabPanel>
-      </div>
-    </div>
+      </TabPanels>
+    </Tabs>
   );
 };
