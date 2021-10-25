515d5faf640cd81aaf810b87c5875166e7c1fa90
yamadahiroki <hirockysan1983@gmail.com>
2021-10-26 08:56:16 +0900

    ✨ Questionページのユーザーデータの処理実装

--- src/pages/question.tsx
+++ src/pages/question.tsx
@@ -1,19 +1,47 @@
-import Head from "next/head";
-import { SubHeader } from "../layouts/header/SubHeader";
-
-import React from "react";
+import React, { useEffect } from "react";
 import { Layout } from "../layouts/Layout";
 import { TabMenu } from "../layouts/contents/TabMenu";
 import { NextPage } from "next";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "../lib/auth";
+import { auth } from "../../firebase";
+import Auth from "./auth";
 
 const Question: NextPage = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unSub();
+    };
+  }, [dispatch]);
   return (
     <>
-      <Layout title="Question">
-        <div className="w-full">
-          <TabMenu  />
-        </div>
-      </Layout>
+      {user.uid ? (
+        <>
+          <Layout title="Question">
+            <div className="w-full">
+              <TabMenu />
+            </div>
+          </Layout>
+        </>
+      ) : (
+        <Auth />
+      )}
     </>
   );
 };
