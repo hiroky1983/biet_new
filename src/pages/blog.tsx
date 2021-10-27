bc6b898f0d7662f48dad80b4e24469617008ceef
yamadahiroki <hirockysan1983@gmail.com>
2021-10-27 23:02:54 +0900

    ✨ Blogページの見た目変更

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,21 @@
+import { Center, Heading } from "@chakra-ui/layout";
 import { NextPage } from "next";
 import Head from "next/head";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react-transition-group/node_modules/@types/react";
+import { auth } from "../../firebase";
 
 import { Layout } from "../layouts/Layout";
+import { login, logout, selectUser } from "../lib/auth";
 
 const Blog: NextPage = () => {
+
   return (
     <div>
       <Layout title="Blog">
-        
+        <Center>
+          <Heading>coming soon....</Heading>
+        </Center>
       </Layout>
     </div>
   );
