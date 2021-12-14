95f2a78f0d2916b233fb0c98d94ea82180745134
yamadahiroki <hirockysan1983@gmail.com>
2021-12-15 00:33:32 +0900

    ✨ Questionコンポーネントの機能を更新

--- src/layouts/contents/TabMenu.tsx
+++ src/layouts/contents/TabMenu.tsx
@@ -1,28 +1,41 @@
 import { useState, VFC } from "react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel, Box } from "@chakra-ui/react";
 import { PostCard } from "./PostCard";
+import { Posts } from "../posts/Posts";
 
 export const TabMenu: VFC = () => {
   const [posts, setPosts] = useState<Array<any>>([]);
   return (
-    <Tabs isFitted variant="enclosed">
-      <TabList mb="1em">
-        <Tab>質問を投稿する</Tab>
-        <Tab>質問に回答する</Tab>
+    <Tabs isFitted variant="enclosed" mr="8">
+      <TabList>
+        <Tab
+          fontWeight="bold"
+          textColor="gray.700"
+          _selected={{ bg: "#3C82F6", color: "white" }}
+        >
+          質問を投稿する
+        </Tab>
+        <Tab
+          fontWeight="bold"
+          p="3"
+          textColor="gray.700"
+          _selected={{ bg: "#3C82F6", color: "white" }}
+        >
+          質問に回答する
+        </Tab>
       </TabList>
       <TabPanels>
-        <TabPanel>
-          <Box>
-            <PostCard />
-            <PostCard />
-          </Box>
+        <TabPanel p="0" h="100vh">
+          <Posts />
         </TabPanel>
-        <TabPanel>
-          {posts.length === 0 ? (
+        <TabPanel p="0" h="100vh">
+          {/* {posts.length === 0 ? (
             <p className="text-gray-700">質問はまだありません</p>
-          ) : (
+          ) : ( */}
             <PostCard />
-          )}
+            <PostCard />
+            <PostCard />
+          {/* )} */}
         </TabPanel>
       </TabPanels>
     </Tabs>
